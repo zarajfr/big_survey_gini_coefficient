f41f016094ba2d3a03d1eff49e28fc98bd9d18c5
Zahra <zahrajafari290@gmail.com>
2022-12-12 11:06:19 +0000

    add to project

---
+++ multiprocess_.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+from random import shuffle
+import math
+from pprint import pprint
+import csv
+import seaborn as sns
+import matplotlib.gridspec as gridspec
+import matplotlib.colors as colors
+# import geopandas as gpd
+import pandas as pd
