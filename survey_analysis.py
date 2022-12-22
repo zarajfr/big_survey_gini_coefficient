985380af6669067ddb4c8df393d80ac38bf3a1a8
Zahra <zahrajafari290@gmail.com>
2022-12-22 16:14:51 +0000

    Update code

--- survey_analysis.py
+++ survey_analysis.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import random
 import math
@@ -12,8 +13,12 @@ import io
 import glob
 import gc
 import multiprocess_ as mp
+import geopandas as gpd
+from shapely.geometry import Point, Polygon
+
 
 def function1(df_cut):
+    # Education_Level
     temp = []
     for i in range(len(df_cut)):
         if (df_cut.iloc[i]["HIGHEST_EDUCATION_2"] == 1):
@@ -33,5 +38,8 @@ def function1(df_cut):
 if __name__ == '__main__':
 
     df_survey = pd.read_csv("df_cut.csv", delimiter = ',', header = 0)
-    df_result = dataframe_multiprocess(function = function1, data_frame = df_survey)
-    print(df_result.head())
+    df_result_1 = mp.dataframe_multiprocess(function = function1, data_frame = df_survey)
+
+    # df_result_1.to_csv('df_cut_1.csv')
+    # print(df_result_2.head(2))
+    # print(df_result_2.columns)
